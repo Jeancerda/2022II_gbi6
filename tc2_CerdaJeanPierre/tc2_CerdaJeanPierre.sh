9d2f6ccb25646e19271781962489cb92e0897e89
Cerda_JPierre_Biotech <115672970+Jeancerda@users.noreply.github.com>
2022-11-09 19:01:34 -0500

    Delete tc2_CerdaJeanPierre directory

--- tc2_CerdaJeanPierre/tc2_CerdaJeanPierre.sh
+++
@@ -1,7 +0,0 @@
-echo "7 primeras filas"
-echo -e "\n"
-head -n 7 EigenBetw.tsv 
-
-echo "5 ultimas filas "
-echo -e "\n"
-tail -n 5 EigenBetw.tsv
--- tc2_CerdaJeanPierre/tc2_CerdaJeanPierre2.sh
+++
@@ -1,5 +0,0 @@
-echo "registros que terminan en id 5 "
-
-echo -e "\n"
-
-grep  AGAP[0-9]*5, EigenBetw.tsv | sort -t "," -k 3 -g > EigenBetw5.csv
